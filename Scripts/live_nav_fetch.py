97199a3e5bb50bcc3ff028af97b9ad4be6d74404
Prashwin Prabhakar <pkj@Prashwins-MacBook-Air.local>
2026-06-04 12:23:48 +0530

    Day 1: Data ingestion complete

---
+++ Scripts/live_nav_fetch.py
@@ -0,0 +1,11 @@
+git init
+
+git add .
+
+git commit -m "Day 1: Data ingestion complete"
+
+git branch -M main
+
+git remote add origin https://github.com/Prashwinachya/bluestock_mf_capstone.git
+
+git push -u origin main
